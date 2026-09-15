690bc6615201078dabe3c2670a43b0f2cab71368
Kiều Vân Sơn <sonkieuvan@gmail.com>
2026-09-15 23:01:19 +0700

    fix: remove redundant CrossOrigin import in userController

--- backend/src/main/java/com/taskly/backend/controller/userController.java
+++ backend/src/main/java/com/taskly/backend/controller/userController.java
@@ -2,6 +2,7 @@ package com.taskly.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.taskly.backend.dto.request.APIrespone;
 import com.taskly.backend.dto.request.LoginRequest;
